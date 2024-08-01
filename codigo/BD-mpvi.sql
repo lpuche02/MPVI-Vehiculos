9fd5d998b7d2b48ce0afb5d0a47785fc131fd7a3
Luis Emilio Puche Aguirre <e.puche02@gmail.com>
2024-07-31 20:36:12 -0500

    Add files via upload

---
+++ codigo/BD-mpvi.sql
@@ -0,0 +1,31 @@
+-- --------------------------------------------------------
+-- Host:                         190.90.160.172
+-- Versión del servidor:         10.6.18-MariaDB-cll-lve-log - MariaDB Server
+-- SO del servidor:              Linux
+-- HeidiSQL Versión:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando datos para la tabla credicel_cotizador_MPVI.mpvi_joy: ~0 rows (aproximadamente)
+DELETE FROM `mpvi_joy`;
+/*!40000 ALTER TABLE `mpvi_joy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mpvi_joy` ENABLE KEYS */;
+
+-- Volcando datos para la tabla credicel_cotizador_MPVI.mpvi_onix: ~0 rows (aproximadamente)
+DELETE FROM `mpvi_onix`;
+/*!40000 ALTER TABLE `mpvi_onix` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mpvi_onix` ENABLE KEYS */;
+
+-- Volcando datos para la tabla credicel_cotizador_MPVI.mpvi_tracker: ~0 rows (aproximadamente)
+DELETE FROM `mpvi_tracker`;
+/*!40000 ALTER TABLE `mpvi_tracker` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mpvi_tracker` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
